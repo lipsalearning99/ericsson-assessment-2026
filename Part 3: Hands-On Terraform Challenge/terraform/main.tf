143deae40dfa1e82d50fd808fb58ed283309793e
EC2 Default User <ec2-user@ip-172-31-67-123.ec2.internal>
2026-02-23 13:17:06 +0000

    lambda function

--- Part 3: Hands-On Terraform Challenge/terraform/main.tf
+++ Part 3: Hands-On Terraform Challenge/terraform/main.tf
@@ -5,9 +5,6 @@ module "lambda_container" {
   ecr_image_uri      = var.ecr_image_uri
   vpc_id             = var.vpc_id
   private_subnet_ids = var.private_subnet_ids
-  environment_variables = {
-    ENVIRONMENT = "prod"
-    LOG_LEVEL   = "info"
-  }
+  environment_variables = var.environment_variables
   tags               = var.tags
 }
